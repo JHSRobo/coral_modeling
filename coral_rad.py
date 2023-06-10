82cc655f17d7feff6d3b80c3d0ad6601e3f5c9d5
James-Randall-14 <jammerand14@gmail.com>
2023-06-09 20:37:36 -0700

    minor patch

--- coral_rad.py
+++ coral_rad.py
@@ -9,7 +9,7 @@ def add_clicks(event,x,y,flags,param):
 
 clicks = []
 
-img = cv2.imread('\\wsl.localhost\Ubuntu-20.04\home\jhsrobo\ROVMIND\ros_workspace\src\img_capture\img\coral\radius\rad.png', cv2.IMREAD_COLOR)
+img = cv2.imread('\home\jhsrobo\ROVMIND\ros_workspace\src\img_capture\img\coral\radius\rad.png', cv2.IMREAD_COLOR)
 cv2.namedWindow(window_name)
 cv2.setMouseCallback(window_name, add_clicks)
 
@@ -20,7 +20,7 @@ while True:
        img = cv2.circle(img,(click[0],click[1]),10,(255,0,0),-1)
     if len(clicks) == 4:
         reference = abs(clicks[0][0] - clicks[1][0])
-        ratio = 40. / reference
+        ratio = 40 / reference
         total = ((((abs(clicks[2][0] - clicks[3][0]) ** 2) + (abs(clicks[2][1] - clicks[3][1])) ** 2)) ** 0.5) * ratio
         displayImg = np.zeros((512,1024,3), dtype=np.uint8)
         cv2.putText(displayImg, "{:.2f} cm".format(total), (50,325), cv2.FONT_HERSHEY_SIMPLEX, 5, (50, 255, 50), 3)
